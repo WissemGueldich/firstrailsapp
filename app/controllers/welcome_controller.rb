c4228ea9fa94fb86d25ea372fb7ce8284a383478
Wissem Gueldich <gueldich.wissem@esprit.tn>
2021-08-16 12:03:04 +0100

    sign up & login

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,4 @@
 class WelcomeController < ApplicationController
   def index
-    flash[:notice] = "logged in successfully"
-    flash[:alert] = "invalid email or password"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
   root to: 'welcome#index'
+
   get "about", to: "about#index"
+
+  get 'sign_up', to: 'registrations#new'
+  post 'sign_up', to: 'registrations#create'
+
+  delete 'logout', to: 'sessions#destroy'
+
+  get 'sign_in', to: 'sessions#new'
+  post 'sign_in', to: 'sessions#create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
